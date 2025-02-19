249c693cc476a8a775683e86dbd10bb3282732d8
Evgeniy Antoshin <antoshin.zhenya74@gmail.com>
2025-02-19 13:07:42 +0500

    update link

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,5 +1,5 @@
 export default {
-  unit6: 'https://www.unit6.dev',
+  unit6: 'https://unit6.ru/',
   github: 'https://github.com/eantoshin',
   telegram: 'https://t.me/evgeniy_antoshin',
   habr: 'https://career.habr.com/edian',
